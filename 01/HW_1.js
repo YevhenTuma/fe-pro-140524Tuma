ab88fa10193578e2ae7c9779b635b36e0bafe681
Євген Тума <tumagroup002@gmail.com>
2024-05-14 22:51:15 +0200

    first commit

---
+++ 01/HW_1.js
@@ -0,0 +1,8 @@
+let userAgeValue = 29;
+let userNameNickname = 'Yevhen';
+console.log(`userAge: ${userAgeValue} userName: ${userNameNickname}`);
+
+userAgeValue = 30;
+userNameNickname = 'Tom';
+
+console.log(`userAge: ${userAgeValue} userName: ${userNameNickname}`);
